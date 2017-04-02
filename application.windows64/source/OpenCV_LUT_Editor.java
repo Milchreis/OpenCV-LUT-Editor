e30fbbb7df25ede779ee5a8672f566230bef6527
Nick <nick-m@gmx.de>
2017-04-02 17:48:52 +0200

    Initial import.

--- application.windows64/source/OpenCV_LUT_Editor.java
+++ application.windows64/source/OpenCV_LUT_Editor.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OpenCV_LUT_Editor extends PApplet {
+
 
 final int MIN = 0;
 final int MAX = 255;
@@ -20,18 +36,18 @@ PImage preview;
 PImage original;
 Integer[] histogram;
 
-void setup() {
+public void setup() {
   lut = initLut(MIN, MAX);
   textSize(10);
   colorMode(RGB, 255);
   textFont(createFont("Monospaced", 11));
 }
 
-void settings() {
+public void settings() {
   size(LUT_WINDOW_WIDTH, LUT_WINDOW_HEIGHT);
 }
 
-void draw() {
+public void draw() {
   background(51);
   
   // Define bar appearance
@@ -90,25 +106,25 @@ void draw() {
   }
 }
 
-void mouseDragged() {
+public void mouseDragged() {
   if(mouseX < LUT_WINDOW_WIDTH) {
     calculateBars(lut);
   }
 }
 
-void mousePressed() {
+public void mousePressed() {
   if(mouseX < LUT_WINDOW_WIDTH) {
     calculateBars(lut);
   }
 }
 
-void mouseReleased() {
+public void mouseReleased() {
   if(preview != null) {
     preview = useLut(original, colorState);
   }
 }
 
-void keyPressed() {
+public void keyPressed() {
   if (key == ' ') {
     selectFolder("Select a folder to process:", "folderSelected");
   }
@@ -147,7 +163,7 @@ void keyPressed() {
   }
 }
 
-void fileSelected(File selection) {
+public void fileSelected(File selection) {
   if (selection != null) {
     // Load image
     original = loadImage(selection.getAbsolutePath());
@@ -159,9 +175,107 @@ void fileSelected(File selection) {
   }  
 }
 
-void folderSelected(File selection) {
+public void folderSelected(File selection) {
   if (selection != null) {
     File file = new File(selection, "lut.py");
     export(file.getAbsolutePath(), lut);
+  }
+}
+
+public void export(String filename, Integer[] lut) {
+  PrintWriter output = createWriter(filename);
+ 
+  output.print("lut_sat =  np.array([");
+  
+  for(int i=0; i<=MAX; i++) {
+    output.print(lut[i]);
+         
+    if(i < MAX) { 
+     output.print(", ");
+    }
+    
+    // Add a newline
+    if(i > 0 && i % 10 == 0) {
+      output.println("");
+      output.print("\t");
+    }
+  }
+  output.print("]).astype('uint8')");
+  output.flush();
+  output.close();
+}
+
+public Integer[] initLut(int min, int max) {
+  Integer[] lut = new Integer[MAX+1];
+  for(int i=min; i<=max; i++) {
+    lut[i] = i;
+  } 
+  return lut;
+}
+
+public PImage useLut(PImage original, int state) {
+  PImage preview = original.copy();
+  preview.loadPixels();
+  for (int i = 0; i < preview.pixels.length-1; i++) {
+          
+    int r = (int) red(preview.pixels[i]);
+    int g = (int) green(preview.pixels[i]);
+    int b = (int) blue(preview.pixels[i]);
+    
+    if(state == CS_ALL) {
+      preview.pixels[i] = color(lut[r], lut[g], lut[b]);
+    }
+    
+    if(state == CS_RED) {
+      preview.pixels[i] = color(lut[r], g, b);
+    }
+    
+    if(state == CS_GREEN) {
+      preview.pixels[i] = color(r, lut[g], b);
+    }
+     
+    if(state == CS_BLUE) {
+      preview.pixels[i] = color(r, g, lut[b]);
+    }
+    
+  }
+  preview.updatePixels();
+  return preview;
+}
+
+public void calculateBars(Integer[] lut) {
+  int barIndex = (int) map(mouseX, 0, 509, MIN, MAX);
+  barIndex = constrain(barIndex, MIN, MAX);
+
+  int value = (int) map(LUT_WINDOW_HEIGHT-mouseY, 0, 509, MIN, MAX);
+  value = constrain(value, MIN, MAX);
+  
+  lut[barIndex] = value;
+  
+  if(barNeigbour > 0) {
+    for(int i=barNeigbour; i > 0; i--) {
+      int neighbourLeft  = constrain(barIndex - i, MIN, MAX);
+      int neighbourRight = constrain(barIndex + i, MIN, MAX);
+       lut[neighbourLeft] = value;
+       lut[neighbourRight] = value;
+    }
+  }
+}
+
+public void smooth(Integer[] lut) {
+  for(int i=0; i<lut.length; i++) {    
+    int neighbourL = constrain(i-1, 0, lut.length);
+    int neighbourR = constrain(i+1, 0, lut.length);
+    lut[i] = (neighbourL + lut[i] + neighbourR)/3;
+  }
+}
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OpenCV_LUT_Editor" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
